9841fe6e7b8068de78c86c16ec41118252e8b30c
Ainur Zamaletdinov <ainur.zamaletdinov.gdc@fujitsu.com>
2023-02-27 19:00:50 +0000

    1

---
+++ src/product/products.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../app/product';
+import { ProductService } from '../app/product.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getproducts();
+  }
+
+  getproducts(): void {
+    this.productService.getProducts()
+    .subscribe(product => this.products = product);
+  }
+
+  
+}
